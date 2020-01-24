0f5bda4170e947296ec715a1dff2ec22a813a144
Ash Yusufzai <ash@technochimp.co.uk>
2020-01-24 11:26:56 +0000

    user list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import './styles.scss';
+import React, { useEffect, useState } from 'react'
+import { getUsers } from './services/Users'
+import './styles.scss'
+
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    getUsers()
+      .then(resp => resp.json())
+      .then(resp => setUsers(resp))
+  }, [])
+
   return (
     <div className="app">
       <header className="app-header">
       </header>
+
+      {!!users.length &&
+        <select>
+          {users.map(user =>
+            <option value={user.name}>{user.name}</option>
+          )}
+        </select>}
     </div>
-  );
+  )
 }
 
 export default App;
